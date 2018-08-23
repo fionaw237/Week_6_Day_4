c2fde7516586a22a03c97cb30eaac38398b290f8
Fiona Wilson <fionaw237@gmail.com>
2018-08-23 18:10:40 +0100

    Adds basic flexbox layout to reading list

--- lab_events_start/js/app.js
+++ lab_events_start/js/app.js
@@ -8,21 +8,56 @@ document.addEventListener('DOMContentLoaded', () => {
   deleteAllButton.addEventListener('click', handleDeleteAllButton)
 });
 
+// ------ before adding flexbox
+// const handleFormSubmit = function(event){
+//   event.preventDefault();
+//
+//   const newListItem = document.createElement('li')
+//   newListItem.textContent = `${event.target.title.value}, ${event.target.author.value}, ${event.target.category.value}`
+//
+//   document.getElementById("new-item-form").reset();
+//
+//   const readingList = document.querySelector('#reading-list');
+//   readingList.appendChild(newListItem);
+// }
 
+
+// ------ after adding flexbox
 const handleFormSubmit = function(event){
   event.preventDefault();
 
-  const newListItem = document.createElement('li')
-  newListItem.textContent = `${event.target.title.value}, ${event.target.author.value}, ${event.target.category.value}`
+// create new grid row for flex box
+  const newGridRow = document.createElement('div')
+  newGridRow.classList.add('grid-row')
+
+// add individual cells for each property
+  const newBookTitle = document.createElement('div')
+  newBookTitle.classList.add('cell');
+  newBookTitle.textContent = `Title: ${event.target.title.value}`
+
+  newGridRow.appendChild(newBookTitle);
+
+  const newBookAuthor = document.createElement('div')
+  newBookAuthor.classList.add('cell');
+  newBookAuthor.textContent = `Author: ${event.target.author.value}`
+
+  newGridRow.appendChild(newBookAuthor);
+
+  const newBookCategory = document.createElement('div')
+  newBookCategory.classList.add('cell');
+  newBookCategory.textContent = `Category: ${event.target.category.value}`
+
+  newGridRow.appendChild(newBookCategory);
 
   document.getElementById("new-item-form").reset();
 
+//add the whole grid row to the reading list
   const readingList = document.querySelector('#reading-list');
-  readingList.appendChild(newListItem);
+  readingList.appendChild(newGridRow);
 }
 
 
 const handleDeleteAllButton = function(){
   const readingList = document.querySelector('#reading-list');
-  readingList.textContent = ""
+  readingList.innerHTML = ""
 }
