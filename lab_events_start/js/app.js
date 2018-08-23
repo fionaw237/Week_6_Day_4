e7542ee915babd26000173e3cb146f114c89a045
Fiona Wilson <fionaw237@gmail.com>
2018-08-23 15:52:26 +0100

    Adds solution to MVP and first extension

--- lab_events_start/js/app.js
+++ lab_events_start/js/app.js
@@ -1,3 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript loaded');
-})
+  // console.log('JavaScript loaded');
+  const form = document.querySelector('#new-item-form');
+  // console.dir(form);
+  form.addEventListener('submit', handleFormSubmit);
+
+  const deleteAllButton = document.querySelector('#delete-all');
+  deleteAllButton.addEventListener('click', handleDeleteAllButton)
+});
+
+
+const handleFormSubmit = function(event){
+  event.preventDefault();
+
+  const newListItem = document.createElement('li')
+  newListItem.textContent = `${event.target.title.value}, ${event.target.author.value}, ${event.target.category.value}`
+
+  document.getElementById("new-item-form").reset();
+
+  const readingList = document.querySelector('#reading-list');
+  readingList.appendChild(newListItem);
+}
+
+
+const handleDeleteAllButton = function(){
+  const readingList = document.querySelector('#reading-list');
+  readingList.textContent = ""
+}
